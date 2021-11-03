937b0187772f406d18e89d9fe2b3d9f9992a8949
Pedro Bufulin <pedro.almeida@ufu.br>
2021-11-03 17:23:09 +0000

    feat(front-end): checks auth

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from 'react';
 import { useMoralis } from 'react-moralis';
 
 export default function Home() {
-  const { enableWeb3, isWeb3Enabled, authenticate, isAuthenticated, logout } =
-    useMoralis();
+  const {
+    enableWeb3,
+    isWeb3Enabled,
+    authenticate,
+    isAuthenticated,
+    logout,
+    isAuthenticating
+  } = useMoralis();
+
+  useEffect(() => {
+    if (isAuthenticated) enableWeb3();
+  }, [isAuthenticated]);
 
   async function handleAuth() {
     if (!isWeb3Enabled || !isAuthenticated) {
@@ -16,7 +27,7 @@ export default function Home() {
   return (
     <>
       <p> front basicão</p>
-      <button onClick={handleAuth} type="button">
+      <button onClick={handleAuth} type="button" disabled={isAuthenticating}>
         {!isWeb3Enabled || !isAuthenticated ? 'logar com metamask' : 'logout'}
       </button>
     </>
