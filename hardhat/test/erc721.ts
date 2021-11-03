67c452bc3d9c0ff8e5915fe840d3e4747c570224
Gustavo Candido <gustavo.candido@pipefy.com>
2021-11-03 19:46:48 -0300

    Types and erc721

--- hardhat/test/erc721.ts
+++ hardhat/test/erc721.ts
@@ -1,12 +1,21 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { Article } from "../types";
 
 describe("Collectible", function () {
+  let article;
+
   it("Should deploy ERC721", async function () {
     const Article = await ethers.getContractFactory("Article");
-    const article = await Article.deploy();
+    article = await Article.deploy();
+    // const article = await Article.deploy();
+
     await article.deployed();
 
     expect(article).to.not.be.undefined;
+  });
+
+  it("Should mint", async function () {
+
   });
 });
