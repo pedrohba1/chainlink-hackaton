fc88938b3abcb89398cd4ae0175c3b1bb72e764c
Pedro Bufulin <pedro.almeida@ufu.br>
2021-11-03 16:57:16 +0000

    feat(front-end): login with metamask

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,7 +1,24 @@
+import { useMoralis } from 'react-moralis';
+
 export default function Home() {
+  const { enableWeb3, isWeb3Enabled, authenticate, isAuthenticated, logout } =
+    useMoralis();
+
+  async function handleAuth() {
+    if (!isWeb3Enabled || !isAuthenticated) {
+      enableWeb3();
+      await authenticate();
+    } else {
+      logout();
+    }
+  }
+
   return (
     <>
       <p> front basicão</p>
+      <button onClick={handleAuth} type="button">
+        {!isWeb3Enabled || !isAuthenticated ? 'logar com metamask' : 'logout'}
+      </button>
     </>
   );
 }
