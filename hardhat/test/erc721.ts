5ea119b12670c159990c0bbb66186d1d40761bda
Pedro Bufulin <pedro.almeida@ufu.br>
2021-11-04 00:50:19 +0000

    tipagem do contrato funcionando

--- hardhat/test/erc721.ts
+++ hardhat/test/erc721.ts
@@ -1,13 +1,13 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { Article } from "../typechain";
 
 describe("Collectible", function () {
-  let article;
+  let article: Article;
 
   it("Should deploy ERC721", async function () {
     const Article = await ethers.getContractFactory("Article");
     article = await Article.deploy();
-    // const article = await Article.deploy();
 
     await article.deployed();
 
