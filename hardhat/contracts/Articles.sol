ce62ac1e6c282e45dc1f1185cd1745adb3ba8a36
Pedro Bufulin <pedro.almeida@ufu.br>
2021-11-05 05:21:38 +0000

    changed to Ids

--- hardhat/contracts/Articles.sol
+++ hardhat/contracts/Articles.sol
@@ -7,10 +7,10 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 contract Articles is ERC1155 {
 
     mapping(uint256  => string) private _uris; 
-    uint256 public articleCounter;
+    uint256 public articleIds;
 
     constructor() public ERC1155("Articles") {
-        articleCounter = 0;
+        articleIds = 0;
     }
 
     function uri(uint256 tokenId) override public view returns (string memory){
@@ -18,7 +18,7 @@ contract Articles is ERC1155 {
     }
 
     function createCollectible(uint256 amount, string memory _uri ) public {
-    _mint(msg.sender, articleCounter, amount, "");
-    _uris[articleCounter] = _uri;
+    _mint(msg.sender, articleIds, amount, "");
+    _uris[articleIds] = _uri;
     }
 }
